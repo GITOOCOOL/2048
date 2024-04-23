72daf461671df2cd21940cb0e417078805ec50c8
Suraj Thakuri <thakurisuraj38@gmail.com>
2024-04-23 16:42:34 +1000

    in the process of implementing generateNewTiles logic, the code is at a minor error right now

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
           tmp[randpos1x][randpos1y] = 2
           tmp[randpos2x][randpos2y] = 2
           setGrid(tmp);
-          console.log(grid);
+          // console.log(grid);
     }
     else{
       setGrid(grid);
@@ -34,7 +34,7 @@ const App = () => {
   }
 
   useEffect(() => {
-    console.log('I run on first render');
+    // console.log('I run on first render');
     updateGrid();
   },[]);
 
@@ -43,49 +43,136 @@ const App = () => {
     handle(e.code)
   }
 
+  // I already figured out how to do leftMove and rightMove in the 1D version
+  // To be able to do upMove and downMove in the 2D version, I need to figure out the way to transform the grid or more correctly rotate the grid clockwise and counter clockwise and the same version of the leftMove and rightMove can be used on the transformed grid. Then finally I can reverse the transformation and get the desired result.
+
+  //
+
 
   // Now I need to update the handle function to incorporate the key up down and the whole 2D sheninigans
   const handle = (key) => {
     // e.preventDefault();
 
     // thankfully, just reversing the tiles array works 
-    let tempTiles = key === 'ArrowLeft'? tiles:tiles.reverse();
+    // let tempTiles = key === 'ArrowLeft'? tiles:tiles.reverse();
     // maybe just iterate over the tiles and push only the populated tiles to a temporary array then at last just push 0's at the end to make it a 1by4 array that will shift the array easily
 
+    //Rather than using the condition for just left and right, I will use a switch case statement for each key presses
 
-    let populatedTiles = tempTiles.filter(t => t > 0)
+    switch (key) {
+      case 'ArrowLeft':
+      {
+          let generatingStack = grid.map((stack)=>{
+            if(stack.reduce((acc, stack)=> ))
+          })
+          console.log(key);
+          let tempGrid = grid
+          // console.log(tempGrid);
+          tempGrid = tempGrid.map((stack,index=0)=> {
+              let tempStack = stack
+              // console.log(tempStack);
+              let populatedTiles = tempStack.filter(t => t>0)
+              
+              for(let i = 0; i < 4; i++){
+                if(i < populatedTiles.length){
+                  continue; //
+                }
+                else {
+                  populatedTiles.push(0)//by pushing the 0s at the end we basically completed the shift
+                }
+              }
+              
+              // logic to merge two tiles with the same values
+              let shiftedStack = populatedTiles;
+              for(let i = 0; i < shiftedStack.length-1; i++) {// we only iterate over the first 3 tiles to prevent the loop getting out of the array bounds
+                if(shiftedStack[i+1] === shiftedStack[i]) {
+                  shiftedStack[i] *= 2
+                  shiftedStack[i+1] = 0;
+                }
+              }
+              
+              // after we merged the tiles, we need to shift the tiles again
+              // so we first filter the populated tiles again and push 0s at the end to make the shift logic happen again
+              // maybe after this I should try to extract the shifting function to make the code more modular
+              shiftedStack = shiftedStack.filter(t => t > 0)
+              
+              for(let i = 0; i < 4; i++){
+                if(i < shiftedStack.length){
+                  continue; //
+                }
+                else {
+                  shiftedStack.push(0)
+                }
+              }
 
-    for(let i = 0; i < 4; i++){
-      if(i < populatedTiles.length){
-        continue; //
-      }
-      else {
-        populatedTiles.push(0)//by pushing the 0s at the end we basically completed the shift
-      }
+              // put a logic to generate the  new tiles in the selected stacks only
+              if(index in generatingStack){
+                console.log('asdfdsafa', index);
+              }
+                // Generating new 2 numbered tiles logic
+                // generateNew2()
+                //find empty cells
+                let emptyCells = [];
+                for(let i = 0; i <shiftedStack.length; i++){
+                  if(shiftedStack[i] === 0){
+                    emptyCells.push(i)
+                  }
+                //randomly select an empty cell and populate it with 2
+                let selectedEmptyCell = Math.floor(Math.random() * emptyCells.length);
+                shiftedStack[emptyCells[selectedEmptyCell]] = 2;
+              }
+              
+              // also need to reverse at the end to get the correct tiles
+              // setTiles(key==='ArrowLeft'?shiftedStack.map((t)=>t):shiftedStack.map(t => t).reverse())
+              console.log('asdf: ',shiftedStack);
+              index++;
+              return shiftedStack
+          })
+          console.log('updated grid:', tempGrid);
+          setGrid(tempGrid);
+          // console.log(grid)
     }
+      break;
+      case 'ArrowRight': break;
+      case 'ArrowUp': break;
+      case 'ArrowDown': break;
+        default: break;
+    }
+
+
+    // let populatedTiles = tempTiles.filter(t => t > 0)
+
+    // for(let i = 0; i < 4; i++){
+    //   if(i < populatedTiles.length){
+    //     continue; //
+    //   }
+    //   else {
+    //     populatedTiles.push(0)//by pushing the 0s at the end we basically completed the shift
+    //   }
+    // }
 
     // logic to merge two tiles with the same values
-    let shiftedTiles = populatedTiles;
-    for(let i = 0; i < shiftedTiles.length-1; i++) {// we only iterate over the first 3 tiles to prevent the loop getting out of the array bounds
-      if(shiftedTiles[i+1] === shiftedTiles[i]) {
-        shiftedTiles[i] *= 2
-        shiftedTiles[i+1] = 0;
-      }
-    }
+    // let shiftedTiles = populatedTiles;
+    // for(let i = 0; i < shiftedTiles.length-1; i++) {// we only iterate over the first 3 tiles to prevent the loop getting out of the array bounds
+    //   if(shiftedTiles[i+1] === shiftedTiles[i]) {
+    //     shiftedTiles[i] *= 2
+    //     shiftedTiles[i+1] = 0;
+    //   }
+    // }
 
     // after we merged the tiles, we need to shift the tiles again
     // so we first filter the populated tiles again and push 0s at the end to make the shift logic happen again
     // maybe after this I should try to extract the shifting function to make the code more modular
-    shiftedTiles = shiftedTiles.filter(t => t > 0)
+    // shiftedTiles = shiftedTiles.filter(t => t > 0)
 
-    for(let i = 0; i < 4; i++){
-      if(i < shiftedTiles.length){
-        continue; //
-      }
-      else {
-        shiftedTiles.push(0)
-      }
-    }
+    // for(let i = 0; i < 4; i++){
+    //   if(i < shiftedTiles.length){
+    //     continue; //
+    //   }
+    //   else {
+    //     shiftedTiles.push(0)
+    //   }
+    // }
 
 
     // I don't know why I put this code here, but it maybe because the rerendiring didn't work in chrome and I was trying to force re-rendering, however in safari the code seems to work fine without the below codes
@@ -96,18 +183,18 @@ const App = () => {
     // Generating new 2 numbered tiles logic
     // generateNew2()
     //find empty cells
-    let emptyCells = [];
-    for(let i = 0; i <shiftedTiles.length; i++){
-      if(shiftedTiles[i] === 0){
-        emptyCells.push(i)
-      }
-    }
-    //randomly select an empty cell and populate it with 2
-    let selectedEmptyCell = Math.floor(Math.random() * emptyCells.length);
-    shiftedTiles[emptyCells[selectedEmptyCell]] = 2;
+    // let emptyCells = [];
+    // for(let i = 0; i <shiftedTiles.length; i++){
+    //   if(shiftedTiles[i] === 0){
+    //     emptyCells.push(i)
+    //   }
+    // }
+    // //randomly select an empty cell and populate it with 2
+    // let selectedEmptyCell = Math.floor(Math.random() * emptyCells.length);
+    // shiftedTiles[emptyCells[selectedEmptyCell]] = 2;
 
-    // also need to reverse at the end to get the correct tiles
-    setTiles(key==='ArrowLeft'?shiftedTiles.map((t)=>t):shiftedTiles.map(t => t).reverse())
+    // // also need to reverse at the end to get the correct tiles
+    // setTiles(key==='ArrowLeft'?shiftedTiles.map((t)=>t):shiftedTiles.map(t => t).reverse())
     
 
   }
